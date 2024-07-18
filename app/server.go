a9fb0e4d66c9fe28dada208559f49b0d20074d2d
Nathael Arki <nath.arki36@gmail.com>
2024-07-18 14:24:07 +0200

    stage 2: Respond with 200.

--- app/server.go
+++ app/server.go
@@ -14,9 +14,11 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
