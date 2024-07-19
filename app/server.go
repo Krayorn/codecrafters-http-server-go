daf4fe3cd506d699d309be9378d2d789bcdfcdc9
Nathael Arki <nath.arki36@gmail.com>
2024-07-19 13:23:48 +0200

    stage 9: accept gzip encoding header.

--- app/server.go
+++ app/server.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"compress/gzip"
 	"errors"
 	"fmt"
+	"log"
 	"net"
 	"os"
 	"strconv"
@@ -80,6 +83,21 @@ func listenReq(conn net.Conn) {
 			return
 		}
 		content := uriParts[2]
+
+		if encoding, ok := request.Headers["Accept-Encoding"]; ok {
+			if encoding == "gzip" {
+				var b bytes.Buffer
+				gz := gzip.NewWriter(&b)
+				if _, err := gz.Write([]byte(content)); err != nil {
+					log.Fatal(err)
+				}
+
+				contentLength := len(b.Bytes())
+				conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\nContent-Encoding: %s\r\n\r\n%s", contentLength, encoding, b.Bytes())))
+				return
+			}
+		}
+
 		contentLength := utf8.RuneCountInString(content)
 		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", contentLength, content)))
 
