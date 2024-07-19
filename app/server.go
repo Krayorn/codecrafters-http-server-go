41bd458a212277a03d4a15e39002c5ff59279b8c
Nathael Arki <nath.arki36@gmail.com>
2024-07-19 09:54:48 +0200

    stage 7: return file.

--- app/server.go
+++ app/server.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net"
 	"os"
 	"strings"
-	"sync"
 	"unicode/utf8"
 )
 
@@ -14,8 +14,14 @@ type HTTPRequest struct {
 	Url     string
 }
 
+var tempDirectory string
+
 func main() {
 	fmt.Println("Logs from your program will appear here!")
+
+	if len(os.Args) > 2 {
+		tempDirectory = os.Args[2]
+	}
 
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
@@ -23,24 +29,23 @@ func main() {
 		os.Exit(1)
 	}
 
-	var wg sync.WaitGroup
-	for i := 0; i < 10; i++ {
-		wg.Add(1)
-		go listenReq(l)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go listenReq(conn)
 	}
 
-	wg.Wait()
 }
 
-func listenReq(l net.Listener) {
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-
+func listenReq(conn net.Conn) {
 	rawReq := make([]byte, 4096)
 	conn.Read(rawReq)
+
+	defer conn.Close()
 
 	parts := strings.Split(string(rawReq), "\r\n")
 	requestLineParts := strings.Split(parts[0], " ")
@@ -59,27 +64,37 @@ func listenReq(l net.Listener) {
 
 	if request.Url == "/" {
 		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-		conn.Close()
 	} else if strings.HasPrefix(request.Url, "/echo") {
 		uriParts := strings.Split(request.Url, "/")
 		if len(uriParts) > 3 {
 			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-			conn.Close()
+			return
 		}
 		content := uriParts[2]
-		contentLength := utf8.RuneCountInString((content))
+		contentLength := utf8.RuneCountInString(content)
 		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", contentLength, content)))
-		conn.Close()
+
 	} else if strings.HasPrefix(request.Url, "/user-agent") {
 		content := request.Headers["User-Agent"]
-		contentLength := utf8.RuneCountInString((content))
-
+		contentLength := utf8.RuneCountInString(content)
 		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", contentLength, content)))
-		conn.Close()
+	} else if strings.HasPrefix(request.Url, "/files") {
+		uriParts := strings.Split(request.Url, "/")
+		if len(uriParts) > 3 {
+			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+			return
+		}
+
+		path := uriParts[2]
+		if _, err := os.Stat(fmt.Sprintf("/%s/%s", tempDirectory, path)); errors.Is(err, os.ErrNotExist) {
+			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+			return
+		}
+
+		content, _ := os.ReadFile(fmt.Sprintf("/%s/%s", tempDirectory, path))
+		contentLength := utf8.RuneCountInString(string(content))
+		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", contentLength, content)))
 	} else {
 		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-		conn.Close()
 	}
-
-	go listenReq(l)
 }
